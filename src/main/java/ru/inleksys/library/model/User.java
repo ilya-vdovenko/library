9ab0d7fbd82688719aee49a153588c73c95cd9f9
ilya-vdovenko <vdovenko.ilia@yandex.ru>
2019-10-27 14:54:35 +0300

    Add User validations, controller and repository

--- src/main/java/ru/inleksys/library/model/User.java
+++ src/main/java/ru/inleksys/library/model/User.java
@@ -1,12 +1,23 @@
 package ru.inleksys.library.model;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 public class User {
 
+    @NotEmpty(message = "Поле Имя не может быть пустым")
+    @Size(min = 5, max = 30, message = "Длинна поля Имя от {min} до {max} знаков")
     private String Username;
+    @NotEmpty(message = "Поле Пароль не может быть пустым")
+    @Size(min = 8, max = 16, message = "Длинна поля Пароль от {min} до {max} знаков")
     private String Password;
 
-    public User(String username) {
+    public User() {
+    }
+
+    public User(String username, String password) {
         this.Username = username;
+        this.Password = password;
     }
 
     public String getUsername() {
---
+++ src/main/java/ru/inleksys/library/web/UserController.java
@@ -0,0 +1,73 @@
+package ru.inleksys.library.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import ru.inleksys.library.model.User;
+import ru.inleksys.library.repository.UserRepository;
+
+import javax.validation.Valid;
+
+@Controller
+public class UserController {
+
+    private final UserRepository ur;
+
+    @Autowired
+    public UserController(UserRepository ur) {
+        this.ur = ur;
+    }
+
+    @GetMapping("/users")
+    public String getUsers(Model model) {
+        model.addAttribute("user_list", ur.getAllUsers());
+        return "users";
+    }
+
+    @GetMapping("/users/new")
+    public String initCreationForm(Model model) {
+        model.addAttribute("book", new User());
+        return "user_form";
+    }
+
+    @PostMapping("/users/new")
+    public String processCreationForm(@Valid User new_user, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            model.addAttribute("user", new_user);
+            return "user_form";
+        }
+        ur.addUser(new_user);
+        return "redirect:/users";
+    }
+
+    @GetMapping("/users/delete")
+    public String deleteUser(@RequestParam String name) {
+        ur.delUser(name);
+        return "redirect:/users";
+    }
+
+    private String lastName = "";
+
+    @GetMapping("/users/edit")
+    public String initUpdateBookForm(@RequestParam String name, Model model) {
+        User edit_user = ur.findUserByName(name);
+        lastName = name;
+        model.addAttribute(edit_user);
+        return "user_form";
+    }
+
+    //TODO попробовать проверку тут из list
+    @PostMapping("/users/edit")
+    public String processUpdateBookForm(@Valid User edit_user, BindingResult result, Model model) {
+        if (result.hasErrors() & !(edit_user.getUsername().equals(lastName))) {
+            model.addAttribute("user",edit_user);
+            return "user_form";
+        }
+        ur.editUser(edit_user, lastName);
+        return "redirect:/books";
+    }
+}
